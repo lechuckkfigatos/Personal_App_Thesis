b96b6e5e19d4796e161e1a1ece0e9b458f445a5a
JeSuisUnChien <149032087+lechuckkfigatos@users.noreply.github.com>
2025-06-21 21:59:43 +0700

    Update key.java

--- src/main/java/org/lechuck/personal_app/key.java
+++ src/main/java/org/lechuck/personal_app/key.java
@@ -1,16 +1,1 @@
-//package org.lechuck.personal_app;
-//
-//import java.security.NoSuchAlgorithmException;
-//import javax.crypto.KeyGenerator;
-//
-//import java.util.Base64;
-//
-//public class key {
-//    public static void main(String[] args) throws NoSuchAlgorithmException {
-//        KeyGenerator keyGen = KeyGenerator.getInstance("HmacSHA256");
-//        keyGen.init(256); // Ensure 256-bit key
-//        byte[] key = keyGen.generateKey().getEncoded();
-//        String base64Key = Base64.getEncoder().encodeToString(key);
-//        System.out.println("Generated Key: " + base64Key);
-//    }
-//}
+
